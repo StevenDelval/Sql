4f2ad7c31926923e3f6cd27965cc78cb15c151a5
StevenDelval <asteven59220@free.fr>
2022-07-04 10:00:34 +0200

    Add sql request

--- SQL.py
+++ SQL.py
@@ -3,8 +3,27 @@ import sqlite3
 connexion = sqlite3.connect("Chinook_Sqlite.sqlite")
 curseur = connexion.cursor()
 
+'''
 curseur.execute("""
-                SELECT * from Artist 
+                SELECT Name from Artist ; 
+""" )
+
+for ligne in  curseur.fetchall() :
+    print(ligne,end="\n")
+'''
+
+
+'''
+curseur.execute("""
+                SELECT Name,Milliseconds  from Track WHERE Milliseconds >500000;
+""" )
+
+for ligne in  curseur.fetchall() :
+    print(ligne,end="\n")
+'''
+
+curseur.execute("""
+                SELECT Genre.Name,Track.Name,Milliseconds  from Track JOIN Genre ON Genre.GenreID = Track.GenreID  WHERE Milliseconds >=300000 and Genre.Name ="Rock";
 """ )
 
 for ligne in  curseur.fetchall() :
